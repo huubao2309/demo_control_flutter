dbc3a35db46c208aaa874f204993b1bccf755215
Huu Bao Nguyen <huubao2309@gmail.com>
2020-02-10 18:03:06 +0700

    commit tabbar

--- control_flutter/lib/main.dart
+++ control_flutter/lib/main.dart
@@ -6,6 +6,8 @@ import 'carousel_control/infinity_scroll_carousel.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  TabController _tabController;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,9 +27,57 @@ class MyApp extends StatelessWidget {
           ),
         ),
         body: Container(
-          child: IndicatorWithCarousel(),
+          child: TabBarView(
+            children: [
+              new Text("This is call Tab View"),
+              new Text("This is chat Tab View"),
+              new Text("This is notification Tab View"),
+            ],
+            controller: _tabController,
+          ),
         ),
+        bottomSheet: HonePageState(_tabController),
+        // bottomSheet: TabBar(
+        //   unselectedLabelColor: Colors.white,
+        //   labelColor: Colors.amber,
+        //   tabs: null,
+        // ),
       ),
+    );
+  }
+}
+
+class HonePageState extends StatefulWidget {
+  @override
+  _HonePageStateState createState() => _HonePageStateState(this._tabController);
+}
+
+class _HonePageStateState extends State<HonePageState>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  _HonePageStateState(this._tabController);
+
+  @override
+  void initState() {
+    _tabController = new TabController(length: 3, vsync: this);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBar(
+      unselectedLabelColor: Colors.yellow,
+      labelColor: Colors.red,
+      tabs: [
+        new Tab(icon: new Icon(Icons.call)),
+        new Tab(
+          icon: new Icon(Icons.chat),
+        ),
+        new Tab(
+          icon: new Icon(Icons.notifications),
+        )
+      ],
+      controller: _tabController,
     );
   }
 }

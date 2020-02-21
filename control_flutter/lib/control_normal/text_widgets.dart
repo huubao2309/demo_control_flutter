fbc334438565d4120445c4e2c221418706c99c64
Huu Bao Nguyen <huubao2309@gmail.com>
2020-02-21 11:45:44 +0700

    add font in flutter

--- control_flutter/lib/control_normal/text_widgets.dart
+++ control_flutter/lib/control_normal/text_widgets.dart
@@ -8,12 +8,14 @@ class TextWidgets extends StatelessWidget {
         alignment: Alignment.center,
         color: Colors.blue,
         child: Text(
-          'Container',
+          'Not Use Font DancingScript',
           textDirection: TextDirection.ltr,
           style: TextStyle(
             color: Colors.black,
             fontSize: 30,
             decoration: TextDecoration.underline,
+            fontFamily: 'DancingScript',
+            fontWeight: FontWeight.w700,
           ),
         ),
       ),
